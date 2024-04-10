3b1f35e938bb58a381ea1e98ed8493849edee047
altiboi <122795024+altiboi@users.noreply.github.com>
2024-04-10 23:42:57 +0200

    api modified

--- api/addCar/index.js
+++ api/addCar/index.js
@@ -1,13 +1,17 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const cars = require("../cars.json");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = async function addCar(context, req) {
+  try {
+    context.log('JavaScript HTTP trigger function adding the whip...');
 
+    const newCar = req.body;
+    cars.push(newCar);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      body: newCar,
     };
-}
+  } catch (error) {
+    context.res = {
+      body: { message: "There has been an error adding car", error },
+    };
+  }
+};
